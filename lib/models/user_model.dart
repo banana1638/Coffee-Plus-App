983840a975a914325d9085e3ac7610bf4a67c30c
Chen Kar Chun <ckc06chun@gmail.com>
2026-03-13 12:28:41 +0800

    update: fix bug

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,31 +1,31 @@
 class User {
-  final int id;
+  final int? id;
   final String name;
   final String email;
   final double balance;
   final int oz;
 
   User({
-    required this.id,
+    this.id,
     required this.name,
     required this.email,
     required this.balance,
     required this.oz,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
+  factory User.fromJson(Map<String, dynamic>? json) {
+    if (json == null) {
+      return User(id: null, name: 'GUEST', email: '', balance: 0.0, oz: 0);
+    }
+
     return User(
       id: json['id'] is int
           ? json['id']
-          : int.tryParse(json['id']?.toString() ?? '') ?? 0,
-      name: json['name'] ?? '',
+          : int.tryParse(json['id']?.toString() ?? ''),
+      name: json['name'] ?? 'GUEST',
       email: json['email'] ?? '',
-      balance: json['balance'] is double
-          ? json['balance']
-          : double.tryParse(json['balance']?.toString() ?? '') ?? 0.0,
-      oz: json['oz'] is int
-          ? json['oz']
-          : int.tryParse(json['oz']?.toString() ?? '') ?? 0,
+      balance: (json['balance'] ?? 0).toDouble(),
+      oz: json['oz'] ?? 0,
     );
   }
 }
